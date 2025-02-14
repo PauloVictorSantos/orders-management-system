fd5b922f076145004d21a831f3eae83d0bdd126d
paulo <paulovictor494@gmail.com>
2025-02-13 21:00:17 -0300

    @Lock(value = LockModeType.PESSIMISTIC_WRITE)

--- src/main/java/com/moutti/orders/repository/PedidoRepository.java
+++ src/main/java/com/moutti/orders/repository/PedidoRepository.java
@@ -1,11 +1,18 @@
 package com.moutti.orders.repository;
 
 import com.moutti.orders.model.Pedido;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
+    @Override
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    Optional<Pedido> findById(Long aLong);
 
 }
